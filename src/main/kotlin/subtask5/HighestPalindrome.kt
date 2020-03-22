1da8e64a98f3db0ca76e2c901ca1581e708217af
Nikita <mynewacc.create@gmail.com>
2020-03-22 18:00:51 +0300

    5th subtask completed and tested

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,50 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        var mismatch = 0
+        for (i in 0 until n/2)
+            if (digitString[i] != digitString[digitString.lastIndex - i])
+                if(++mismatch > k)
+                    return "-1"
+
+        val copyString = digitString.toMutableList()
+
+        if (k >= n) {
+            for (i in digitString.indices)
+                if (digitString[i] != '9')
+                    copyString[i] = '9'
+        } else {
+            mismatch = k
+            for (i in 0 until n / 2) {
+                if (digitString[i] > digitString[digitString.lastIndex - i]) {
+                    copyString[copyString.lastIndex - i] = copyString[i]
+                    mismatch--
+                } else if (digitString[i] < digitString[digitString.lastIndex - i]) {
+                    copyString[i] = copyString[copyString.lastIndex - i]
+                    mismatch--
+                }
+            }
+            var i = 0
+            if (mismatch % 2 == 1)
+                mismatch++
+            while (i < n/2 && mismatch > 0) {
+                if (copyString[i] != '9') {
+                    copyString[i] = '9'
+                    copyString[copyString.lastIndex - i] = '9'
+                    mismatch -= 2
+                }
+                i++
+            }
+            if (n % 2 == 1 && mismatch >= 1)
+                if (copyString[n / 2] != '9')
+                    copyString[n / 2] = '9'
+        }
+
+        val result = StringBuilder()
+        copyString.forEach { result.append(it) }
+        return result.toString()
     }
+
 }
